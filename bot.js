d3e94c8fc9b7aa1b801cf72035a8c27eb8aef301
Geoffery <powellgeoffery@gmail.com>
2020-12-08 17:25:04 -0600

    Fixed Issues

--- bot.js
+++ bot.js
@@ -43,6 +43,10 @@ client.once('ready', () => {
                     IP = body.ip
                 });
         });
+  let rawdata = fs.readFileSync('status.json');
+  let status = JSON.parse(rawdata);
+  client.user.setPresence(status);
+  client.user.setPresence(status)
   console.log('Geoffery\'s Son is Ready!');
 });
 
@@ -50,6 +54,9 @@ client.login(auth.token);
 
 
 client.on('message', message => {
+  let rawdata = fs.readFileSync('status.json');
+  let status = JSON.parse(rawdata);
+  client.user.setPresence(status);
   if (message.author.id == "779431244222955520"){
     if (message.content.includes(" » ") == true) {
       message.content = message.content.substring(message.content.indexOf("»") + 2);
